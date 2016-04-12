e94e1720cf6dbf15180f8e47ff711cb1cd21a5f0
Cotel <miguelcoletomunoz@hotmail.com>
2016-04-12 13:52:20 +0200

    style changes

--- public/main.js
+++ public/main.js
@@ -12,12 +12,12 @@ var socket = io.connect(window.location.host);
 function render(data) {
 	var html = data.map(function(elem, index) {
 		if(elem.usuario == Cookies.get('name')) {
-			return(`<div>
+			return(`<div id="my-message">
 					<strong>${elem.usuario}: </strong>
 					<em id="me">${elem.texto}</em>
 			</div>`)
 		} else {
-			return(`<div id="my-message">
+			return(`<div>
 					<strong>${elem.usuario}: </strong>
 					<em>${elem.texto}</em>
 			</div>`)
